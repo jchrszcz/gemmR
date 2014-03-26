e13cb702d2b7a5b5e9d6f9cb9dab8d2a17e0fb0d
jchrszcz <jchrabaszcz@gmail.com>
2014-03-26 03:16:48 -0400

    replaced solve with lm to avoid fatal errors

--- gemmR/R/gemmquick.R
+++ gemmR/R/gemmquick.R
@@ -178,12 +178,7 @@ gemmEst <- function(input.data, output = "gemmr", n.beta = 8000, p.est = 1,
 
   # Scale coefficients to metric scale
   y.hats <- matrix(input.data[,-1], ncol = p) %*% t(coefficients) 
-  scales <- apply(y.hats,2, function(x) {
-    tmp <- cbind(1,x)
-    y <- input.data[,1]
-    solve(crossprod(tmp))%*%crossprod(tmp,y)
-  })
-  scales <- t(scales)
+  scales <- t(apply(y.hats,2,function(x) coef(lm(y ~ x))))
   coefficients <- cbind(intercept=scales[,1],scales[,2]*coefficients)
 
 
